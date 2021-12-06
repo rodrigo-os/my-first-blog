5bb72135e7140f905a0d5f23ffb298434a95f3d0
Rodrigo Oliveira da Silva <rodrigoods49@gmail.com>
2021-12-06 18:04:42 -0300

    Fix secret key exposed

---
+++ mysite/local_settings.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6$*z=m3s9r$nqp=dxden#toaj65(92pf+i&js7myq59@7w+r*+'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
